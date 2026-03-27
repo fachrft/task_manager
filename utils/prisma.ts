eec7381ac8c4d8f04d9944426ff20e7e4b953c37
Fachrifat Zhafran <fachrifat05@gmail.com>
2026-03-27 23:04:37 +0700

    chore: include generated prisma client in git to fix vercel build

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@/generated";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
 export const prisma = new PrismaClient({ adapter });
